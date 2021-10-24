a0a7a508d9b36f4846474e4535f67baaeab3e749
vlevin <vlevin@azbukavkusa.ru>
2021-10-24 13:25:57 +0400

    Returned test starter

--- src/test/java/com/example/stockAPI/APITest.java
+++ src/test/java/com/example/stockAPI/APITest.java
@@ -1,9 +1,8 @@
 package com.example.stockAPI;
 
 import org.junit.jupiter.api.Test;
-import ru.tinkoff.invest.openapi.OpenApi;
-import ru.tinkoff.invest.openapi.model.rest.SandboxRegisterRequest;
-import ru.tinkoff.invest.openapi.okhttp.OkHttpOpenApi;
+import org.springframework.boot.test.context.SpringBootTest;
+
 
 
 public class APITest {
--- src/test/java/com/example/stockAPI/StockApiApplicationTests.java
+++ src/test/java/com/example/stockAPI/StockApiApplicationTests.java
@@ -1,7 +1,9 @@
 package com.example.stockAPI;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class StockApiApplicationTests {
 
 	@Test
